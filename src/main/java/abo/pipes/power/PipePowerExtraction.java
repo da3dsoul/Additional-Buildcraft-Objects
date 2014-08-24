b764cd45c68138bf898ba07b30db76afc018c5fe
da3dsoul <da3dsoul@gmail.com>
2014-08-24 01:55:45 -0400

    Fix oopsies in Reinforced Wooden Pipe (save button)

--- src/main/java/abo/pipes/power/PipePowerExtraction.java
+++ src/main/java/abo/pipes/power/PipePowerExtraction.java
@@ -1,6 +1,12 @@
 package abo.pipes.power;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.util.ForgeDirection;
+import abo.ABO;
+import abo.PipeIconProvider;
+import buildcraft.api.core.IIconProvider;
 import buildcraft.api.power.IPowerReceptor;
 import buildcraft.transport.IPipeTransportPowerHook;
 import buildcraft.transport.pipes.PipePowerWood;
@@ -10,6 +16,16 @@ public class PipePowerExtraction extends PipePowerWood implements IPowerReceptor
 
 	public PipePowerExtraction(Item item) {
 		super(item);
-		
+	}
+	
+	@Override
+	public int getIconIndex(ForgeDirection direction) {
+		return PipeIconProvider.PipePowerExtraction;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIconProvider getIconProvider() {
+		return ABO.instance.pipeIconProvider;
 	}
 }
